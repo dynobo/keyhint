a9255ce8223a555e82735c2a268b59ec9f63db16
dynobo <dynobo@mailbox.org>
2020-05-15 18:13:49 +0200

    Add readme with screenshots

--- Makefile
+++ Makefile
@@ -1,10 +1,21 @@
 init:
 	rm -rf ./.venv
+	rm ~/.config/keyhint/*
 	python3 -m venv .venv
 	.venv/bin/python -m pip install -r requirements-dev.txt
+
+reset_config:
+	rm ~/.config/keyhint/*
 
 test:
 	.venv/bin/pytest tests
 
+lint:
+	.venv/bin/pylama
+
 run:
 	.venv/bin/python -m keyhint
+
+package:
+	rm -rf build dist
+	python setup.py sdist bdist_wheel
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("LICENSE") as f:
 setup(
     name="keyhint",
     version=keyhint.__version__,
-    description="Show context sensitive keyboard shortcuts or other hints",
+    description="Display context sensitive keyboard shortcuts or other hints",
     keywords="shortcuts keybindings hints helper reminder",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -21,7 +21,8 @@ setup(
     author_email=keyhint.__email__,
     url=keyhint.__repo__,
     license=license_text,
-    packages=find_packages(exclude=("tests", "data")),
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
     entry_points={"console_scripts": ["keyhint=keyhint.__main__:run"]},
     classifiers=[
         "Development Status :: 4 - Beta",
