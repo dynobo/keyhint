0d54bf7b7f5ff1ddfe2eabbfb8cc833c40b1f6ad
dynobo <dynobo@mailbox.org>
2024-04-21 12:13:53 +0200

    feat: verify prerequisites and show banners if missed

--- keyhint/context.py
+++ keyhint/context.py
@@ -6,8 +6,6 @@ import os
 import re
 import shutil
 import subprocess
-import sys
-import traceback
 
 logger = logging.getLogger("keyhint")
 
@@ -19,6 +17,15 @@ def is_using_wayland() -> bool:
         [bool] -- {True} if probably Wayland
     """
     return "WAYLAND_DISPLAY" in os.environ
+
+
+def has_xprop() -> bool:
+    """Check if xprop is installed.
+
+    Returns:
+        [bool] -- {True} if xprop is installed
+    """
+    return shutil.which("xprop") is not None
 
 
 def get_gnome_version() -> str:
@@ -74,7 +81,7 @@ def get_kde_version() -> str:
         return kde_version
 
 
-def get_desktop_environment() -> str:
+def get_desktop_environment_and_version() -> tuple[str, str]:
     """Detect used desktop environment."""
     kde_full_session = os.environ.get("KDE_FULL_SESSION", "").lower()
     xdg_current_desktop = os.environ.get("XDG_CURRENT_DESKTOP", "").lower()
@@ -85,12 +92,15 @@ def get_desktop_environment() -> str:
     if gnome_desktop_session_id == "this-is-deprecated":
         gnome_desktop_session_id = ""
 
-    de = "not detected"
+    de = "(DE not detected)"
+    version = "(version not detected)"
 
     if gnome_desktop_session_id or "gnome" in xdg_current_desktop:
-        de = f"Gnome v{get_gnome_version()}"
+        de = "Gnome"
+        version = get_gnome_version()
     if kde_full_session or "kde-plasma" in desktop_session:
-        de = f"KDE v{get_kde_version()}"
+        de = "KDE"
+        version = get_kde_version()
     if "sway" in xdg_current_desktop or "sway" in desktop_session:
         de = "Sway"
     if "unity" in xdg_current_desktop:
@@ -100,10 +110,20 @@ def get_desktop_environment() -> str:
     if "awesome" in xdg_current_desktop:
         de = "Awesome"
 
-    return de
+    return de, version
 
 
-def get_active_window_info_wayland() -> tuple[str, str]:
+def has_window_calls_extension() -> bool:
+    cmd_introspect = (
+        "gdbus introspect --session --dest org.gnome.Shell "
+        "--object-path /org/gnome/Shell/Extensions/Windows "
+    )
+    stdout_bytes = subprocess.check_output(cmd_introspect, shell=True)  # noqa: S602
+    stdout = stdout_bytes.decode("utf-8")
+    return all(["List" in stdout, "GetTitle" in stdout])
+
+
+def get_active_window_via_window_calls() -> tuple[str, str]:
     """Retrieve active window class and active window title on Wayland.
 
     Inspired by https://gist.github.com/rbreaves/257c3edfa301786e66e964d7ac036269
@@ -145,7 +165,7 @@ def get_active_window_info_wayland() -> tuple[str, str]:
     return wm_class, title
 
 
-def get_active_window_info_x() -> tuple[str, str]:
+def get_active_window_via_xprop() -> tuple[str, str]:
     """Retrieve active window class and active window title on Xorg desktops.
 
     Returns:
@@ -184,45 +204,3 @@ def get_active_window_info_x() -> tuple[str, str]:
         wm_class = match.group("class")
 
     return wm_class, title
-
-
-def detect_active_window() -> tuple[str, str]:
-    """Get class and title of active window.
-
-    Identify the OS and display server and pick the method accordingly.
-
-    Returns:
-        Tuple[str, str]: [description]
-    """
-    wm_class = window_title = ""
-
-    try:
-        if is_using_wayland():
-            wm_class, window_title = get_active_window_info_wayland()
-        else:
-            wm_class, window_title = get_active_window_info_x()
-    except Exception:
-        traceback.print_stack()
-        logger.error(  # noqa: TRY400  # the stacktrace should be before message
-            "Couldn't detect active application window.\n"
-            "KeyHint supports Wayland and Xorg.\n"
-            "For Wayland, the installation of the 'Window Calls' gnome extension is "
-            "required:\nhttps://extensions.gnome.org/extension/4724/window-calls\n"
-            "For Xorg, the 'xprop' command is required. Check your system repository "
-            "to identify its package.\n"
-            "If you met the prerequisites but still see this, please create an issue "
-            "incl. the traceback above on:\nhttps://github.com/dynobo/keyhint/issues"
-        )
-        sys.exit(1)
-
-    logger.debug("Detected wm_class: '%s'.", wm_class)
-    logger.debug("Detected window_title: '%s'.", window_title)
-
-    if "" in [wm_class, window_title]:
-        logger.error(
-            "Couldn't detect active window! Please report this error "
-            "together with information about your OS and display server on "
-            "https://github.com/dynobo/keyhint/issues"
-        )
-
-    return wm_class, window_title
